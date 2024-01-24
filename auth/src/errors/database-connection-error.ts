75f5b6acbdd074d6d49fddf819a987dc45e572a4
Ashutosh Padhy <ashutoshpadhy001@gmail.com>
2024-01-24 13:55:38 +0530

    add authentication

--- auth/src/errors/database-connection-error.ts
+++ auth/src/errors/database-connection-error.ts
@@ -1,13 +1,12 @@
-import { CustomError } from "./custom-error";
+import { CustomError } from './custom-error';
 
 export class DatabaseConnectionError extends CustomError {
   statusCode = 500;
-  reason = "Error connecting to database";
+  reason = 'Error connecting to database';
 
   constructor() {
-    super("Error connecting to db");
+    super('Error connecting to db');
 
-    // as we are extending a built in class
     Object.setPrototypeOf(this, DatabaseConnectionError.prototype);
   }
 
